1f5b90ae690bb543e95ca05e366ce5996dc3e05f
Mateusz Horczak <82621@student.pb.pl>
2023-05-28 00:05:41 +0200

    fitness function with conditions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # source: https://strefaedukacji.pl/przedmioty-w-liceum-wykaz-i-wymiar-godzin-w-konkretnych-klasach-tego
 # -nastolatkowie-ucza-sie-klasach-14-szkoly-ponadpodstawowej/ar/c5-16785765
+import math
 import random
 
 
@@ -68,27 +69,66 @@ class GeneticAlgorithm:
         # operacje selekcji na tablicy chromosomów
         break_time = 0  # ilosc okienek
         break_between_the_same_subject = 0  # jesli danego dnia jest 2 razy ten sam przedmiot, to niech będzie w ciągu
-        max_hours_per_day = 0   # maksymalna ilosc zajec jednego dnia (by byla jak najmniejsza, by rownomierny plan byl)
+        max_school_day_length = 0  # maksymalna dlugosc dnia (by byla jak najmniejsza, by rownomierny plan byl)
 
         # szukanie okienek
         for day, day_schedule in chromosome.items():
-            # resetowanie wartosci
             have_lesson_in_this_day = False
             temp_break_time = 0
             for lesson_hour in day_schedule:
                 if have_lesson_in_this_day is False and lesson_hour != '*':  # jesli pierwsza lekcja tego dnia
                     have_lesson_in_this_day = True
                 if have_lesson_in_this_day is True and lesson_hour == '*':  # jesli zaczal lekcje i ma okienko
-                    temp_break_time += 1    # tymczasowa przerwa (albo koniec zajec albo okienko)
+                    temp_break_time += 1  # tymczasowa przerwa (albo koniec zajec albo okienko)
                 if temp_break_time > 0 and lesson_hour != '*':  # stwierdzenie okienka
                     break_time += temp_break_time
                     temp_break_time = 0
-        return break_time
 
+        # przedmiot rozrzucony w ciagu dnia zamiast ciagiem
+        for day, day_schedule in chromosome.items():
+            check_subjects = {}  # jakie przedmioty ile razy w ciagu dnia
+            for lesson_hour in day_schedule:
+                if lesson_hour not in check_subjects and lesson_hour != '*':
+                    check_subjects.update({lesson_hour: 1})
+                elif lesson_hour != '*':
+                    temp = check_subjects.get(lesson_hour)
+                    check_subjects.update({lesson_hour: temp + 1})
 
+            # pesymistyczna ilosc przerw przedmiotow
+            for subject, value in check_subjects.items():
+                if value > 1:
+                    break_between_the_same_subject = break_between_the_same_subject + value - 1
 
+            # wyliczanie takich przerw
+            for i, lesson_hour in enumerate(day_schedule):
+                if lesson_hour != '*' and i != 11:
+                    if check_subjects.get(lesson_hour) > 1 and lesson_hour == day_schedule[i + 1]:
+                        break_between_the_same_subject -= 1
 
+         # maksymalna dlugosc dnia szkolnego
+        school_days_length = []
+        for day, day_schedule in chromosome.items():
+            first_lesson = -1
+            last_lesson = -1
+            for i, lesson_hour in enumerate(day_schedule):
+                if first_lesson == -1 and lesson_hour != '*':
+                    first_lesson = i
+                if first_lesson != -1 and lesson_hour != '*':
+                    last_lesson = i
+            school_days_length.append(last_lesson - first_lesson + 1)
+        max_school_day_length = max(school_days_length)
 
+        # wyliczenie optymalnej sredniej ilosci godzin w ciagu dnia
+        total_hours = 0
+        for day, day_schedule in chromosome.items():
+            total_hours += sum(1 for subject in day_schedule if subject != '*')
+        optimal_hours_day = (total_hours / len(chromosome))
+        optimal_hours_day = math.ceil(optimal_hours_day)
+
+        # wyliczenie funkcji fitness
+        fitness = 1000 * break_time + 100 * break_between_the_same_subject + max_school_day_length - optimal_hours_day
+
+        return [break_time, break_between_the_same_subject, max_school_day_length, fitness]
 
     def perform_crossover(self):
         # operacje krzyzowania na tablicy chromosomow
@@ -103,7 +143,10 @@ if __name__ == '__main__':
     genetic_algorithm = GeneticAlgorithm(population_size=100)
     first_school_schedule = genetic_algorithm.generate_initial_population()
     fitness_score = genetic_algorithm.perform_selection(first_school_schedule)
-    print(f'{fitness_score} godzin okienka')
+    print(f'ilosc okienek: {fitness_score[0]}')
+    print(f'ilosc dlugich przerw miedzy tymi samymi przedmiotami w ciagu dnia: {fitness_score[1]}')
+    print(f'najdluzszy czas w szkole: {fitness_score[2]}')
+    print(f'Fitness score: {fitness_score[3]}')
     # reszta logiki i dzialania algorytmu genetycznego
     for school_day, school_subjects in first_school_schedule.items():
         print(school_day, school_subjects)
